fe4c87a1418bd08e37370d04536f991fb14c123e
Кулагин Егор <cool-eg@mail.ru>
2024-06-18 23:58:35 +0300

    smth

--- main.py
+++ main.py
@@ -3,23 +3,11 @@ import sys
 import termios
 import tty
 
-# не работает
-def getch():
-    """Функция для посимвольного считывания ввода с терминала."""
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
 k = 'привет\n'
 print(k)
 res = ''
 while True:
-    char = getch()
+    char = sys.stdin.read(1)
     res += char
     if res == k:
         print('good')
